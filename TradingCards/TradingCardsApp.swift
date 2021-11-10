def262dc4f1b501ecc4cc2d53f7cb2203912f1b1
Abdul <AMalik@lcs.on.ca>
2021-11-10 14:10:48 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Abdul Malik on 2021-11-10.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
